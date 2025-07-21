c5eddc9ad60917223f742c3e713a3b54a8f20430
athenadc <athenadelacruzart@gmail.com>
2025-07-22 11:47:48 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,11 +1,15 @@
 //your parameter variables go here!
 let rect_width  = 20;
 let rect_height = 30;
-
+let cherry_xPos = 100;
+let cherry_yPos = 100;
+let cherry_Size = 90;
+let leafx = 80
+let 
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+  // pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
@@ -17,11 +21,41 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(128, 9, 56); //burgundy red
+
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  //rect(40 ,40, rect_width, rect_height);
+  // ellipse(18,32, 28,25);
+  // square(20,20, 50,20);
+  // square(12,20, 20,22)
+
+
+
+  //cherry outline
+  fill(255)
+  noStroke(0)
+  circle(cherry_xPos,100,cherry_xPos)
+  //cherry main
+  fill(255,15,50)
+  circle(cherry_xPos,100,cherry_Size)
+  //cherry highlight
+  fill(255)
+  circle(125,80, 15)
+  circle(130,90, 5)
+
+  //cherry leaf
+ // triangle(80, 160, 45, 120, 85, 100)
+  fill(0,200,150) //teal
+  triangle(leafx, 160, 45, 110, leafx, 100)
+  
 }
 
+function draw(){
+
+  
+
+
+}
 //changes
