bc7865ae156d52c006e44f5db2897df39cf5390d
athenadc <athenadelacruzart@gmail.com>
2025-08-03 01:41:36 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -5,31 +5,35 @@
 let cherry_xPos = 100; //moves cherry by the x axis
 let cherry_yPos = 100; //moves cherry by the y axis
 let cherry_Size = 100; //changes cherry size
-//if cherry size is set at a value LESS than 100, the entire design executes the Cherry Blue Variant.
-//if cherry size is set at a value MORE than 100, the entire design executes the Cherry Pink Variant.
+//if cherry size is set at a value LESS than 50, the entire design executes the Cherry Blue Variant.
+//if cherry size is set at a value MORE than 120, the entire design executes the Cherry Pink Variant.
 //to change the cherry only, change color parameters below.
 let cherry_outSize = cherry_Size+10; //increase or decrease the white outline of the cherry based on cherry_Size
 let cherry_xHighlight = cherry_xPos+20; //attaches the highlight on the cherry
 let cherry_yHighlight = cherry_yPos-20; //attaches the highlight on the cherry
-let cherry_sizeHighlight = cherry_Size-80; //increases or decreases highlight base on the cherry_Size
+let cherry_sizeHighlight = cherry_Size-80; //increases or decreases highlight base on the cherry_Size.
 
 //background motifs
-let showDiamond = true; //diamond motif appears
+let showDiamond = false; //diamond motif appears
 let diamondStrokeWeight = 5; //thicker diamond lines
-//diamond lines makes up the diamond motif
+//diamond lines makes up the diamond motif.
 let diamond1Line = 1;
 let diamond2Line = 200;
+//these two triangles can be added if suited.
+let leftTriangle = false; //displays the left triangle
+let rightTriangle = false; //displays the right triangle
 
 //clouds
+let cloudSize = 50;
 let showOutline = true; //displays cloud outlines
 let cloudOutline = 255; //changes outline colour between black and white
-let cloudColour = [52, 232, 235]; //RGB values; use Google Color Picker :)
+let cloudColour = [148, 189, 255]; //RGB values; use Google Color Picker :)
 
 //mountains
-let show_Mountains = true; //displays mountains
+let show_Mountains = false; //displays mountains
 let noOutline = true; //displays mountain outline
-let outlineWeight = 5 //thickness of outlines
-let outlineColour = 255 // between black and white
+let outlineWeight = 5; //thickness of outlines
+let outlineColour = 255; // between black and white
 let arcxPos = 10;
 let arcyPos = 200;
 let arcWidth = 40;
@@ -68,23 +72,23 @@ function wallpaper_background() {
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   
 
-//  noStroke(0);
-//  fill(156, 26, 78);
-//   circle(150,130, 125);
-
-   //clouds
+ //background circle 
+   noStroke(0);
+   fill(156, 26, 78);
+   circle(150,130, 125);
+ 
+ //clouds
    fill(cloudColour);
    noStroke(0);
-   
 
    if (showOutline){
-    stroke(cloudOutline);
+    stroke(cloudOutline); 
    }
 
-  ellipse(35,25, 28,25);
-  square(35,25, 50,20);
-  ellipse(180,150, 28,25);
-  square(135,100, 50,20);
+   ellipse(35,25, cloudSize-22,25); //high clouds
+   square(35,25, cloudSize,20); //high clouds
+   ellipse(180,150, cloudSize-22,25); //low clouds
+   square(135,100, cloudSize,20); //low clouds
 
   //diamond
   if (showDiamond){
@@ -94,12 +98,15 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
     line(diamond1Line,diamond2Line, diamond2Line,diamond1Line);
   }
   
-
   //triangle: centre point, lowest point, highest point
   stroke(255);
-  fill(255)
-  triangle(80,100, 1,20, 1,180); //left
-  //triangle(100,80, 180,1, 20,1); //right
+  fill(255);
+  if (leftTriangle){
+   triangle(80,100, 1,20, 1,180); //left
+  }
+  if (rightTriangle){
+   triangle(120,100, 195,180, 195,20); //right
+  }
   if (!showDiamond){
     noFill(0);
   }
@@ -112,13 +119,13 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 
  //Cherry Blue Variant
   if (cherry_Size < 100){
-  cherryBlue = true
- }
+   cherryBlue = true;
+  }
 
  //Cherry Pink Variant
- if (cherry_Size > 100){
-  cherryPink = true
- }
+  if (cherry_Size > 100){
+   cherryPink = true;
+  }
 
   //cherry body
   if (cherryBlue) {
@@ -128,7 +135,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   } else {
     fill(255,15,50); //cherry red
   }
-    circle(cherry_xPos,cherry_yPos,cherry_Size);
+    circle(cherry_xPos,cherry_yPos,cherry_Size); //cherry
 
 
   //cherry highlight
@@ -142,18 +149,18 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 
 
   //mountains
-    fill(43, 107, 59); //forest green
+  fill(43, 107, 59); //forest green
 
-    if (noOutline){
-      stroke(outlineColour);
-      strokeWeight(outlineWeight);
-    }
+  if (noOutline){
+    stroke(outlineColour);
+    strokeWeight(outlineWeight);
+  }
 
-    if (arcHeight-30){
-      fill(121, 224, 146);
-    }
+  if (arcHeight-30){
+    fill(121, 224, 146);
+  }
 
-    if(show_Mountains){
+  if(show_Mountains){
     arc(arcxPos, arcyPos, arcWidth, arcHeight, arcPi, HALF_PI);
     arc(arcxPos+30, arcyPos, arcWidth, arcHeight-30, arcPi, HALF_PI);
     arc(arcxPos+60, arcyPos, arcWidth, arcHeight, arcPi, HALF_PI);
