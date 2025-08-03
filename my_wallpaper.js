45908e5c23915fe09ef4dfa6d1437d0001fe4176
athenadc <athenadelacruzart@gmail.com>
2025-08-04 00:20:05 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,23 +2,23 @@
 
 
 //cherry
-let cherry_xPos = 140; //moves cherry by the x axis
+let cherry_xPos = 100; //moves cherry by the x axis
 let cherry_yPos = 100; //moves cherry by the y axis
-let cherry_Size = 115; //changes cherry size
+let cherry_Size = 100; //changes cherry size
 //if cherry size is set at a value LESS than 50, the entire design executes the Cherry Blue Variant.
 //if cherry size is set at a value MORE than 120, the entire design executes the Cherry Pink Variant.
 //to change the cherry only, change color parameters below.
-let cherry_outSize = cherry_Size+50; //increase or decrease the white outline of the cherry based on cherry_Size
+let cherry_outSize = cherry_Size+10; //increase or decrease the white outline of the cherry based on cherry_Size
 let cherry_xHighlight = cherry_xPos+20; //attaches the highlight on the cherry
-let cherry_yHighlight = cherry_yPos-25; //attaches the highlight on the cherry
-let cherry_sizeHighlight = cherry_Size-90; //increases or decreases highlight base on the cherry_Size.
+let cherry_yHighlight = cherry_yPos-20; //attaches the highlight on the cherry
+let cherry_sizeHighlight = cherry_Size-80; //increases or decreases highlight base on the cherry_Size.
 
 //background motifs
-let showDiamond = false; //diamond motif appears
+let showDiamond = true; //diamond motif appears
 let diamondStrokeWeight = 5; //changes thickness of diamond lines
 //diamond lines makes up the diamond motif.
-let diamond1Line = 40;
-let diamond2Line = 240;
+let diamond1Line = 1;
+let diamond2Line = 200;
 //these two triangles can be added if suited.
 let leftTriangle = true; //displays the left triangle
 let rightTriangle = true; //displays the right triangle
@@ -31,15 +31,15 @@ let cloudOutline = 255; //changes outline colour between black and white
 let cloudColour = [20, 100, 200]; //RGB values; use Google Color Picker :)
 
 //mountains
-let show_Mountains = false; //displays mountains
+let show_Mountains = true; //displays mountains
 let noOutline = true; //displays mountain outline
-let outlineWeight = 5; //thickness of outlines
-let outlineColour = 5; // between black and white
+let outlineWeight = 2; //thickness of outlines
+let outlineColour = 255; // between black and white
 let arcxPos = 10;
-let arcyPos = 300;
-let arcWidth = 80;
-let arcHeight = 150;
-let arcPi = 10;
+let arcyPos = 200;
+let arcWidth = 40;
+let arcHeight = 100;
+let arcPi = 179;
 
 //color parameters
 let cherryBlue = false; //changes only the main cherry color (overridden when either varients are running).
@@ -49,9 +49,9 @@ let cherryPink = false; //changes only the main cherry color (overridden when ei
 
 function setup_wallpaper(pWallpaper) {
 //pWallpaper.output_mode(DEVELOP_GLYPH);
-pWallpaper.output_mode(GLIDE_WALLPAPER);
+pWallpaper.output_mode(DEVELOP_GLYPH);
   
-  pWallpaper.resolution(NINE_PORTRAIT);
+  pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
